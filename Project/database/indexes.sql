c14fc70b17cab433e254f240c22da64f0ab1e6b1
pedronunomacedo <pedronunomacedo@hotmail.com>
2023-06-08 15:00:33 +0100

    Added reference to LBAW project repo

--- Project/database/indexes.sql
+++
@@ -1,8 +0,0 @@
-CREATE INDEX usersUsername ON User USING hash (userName); -- user username
--- 1) CREATE INDEX userAdresses ON Adress USING hash (id); -- userID
--- 2) CREATE INDEX userPurchases ON Order USING hash (id); -- userID
-CREATE INDEX productReviews ON Review USING hash (id); -- productID
-CREATE INDEX productsCategory ON Product USING hash (id); -- categoryID
-CREATE INDEX searchProducts ON Product USING hash (prodName); -- Product name
-CREATE INDEX orderProductsDESC ON Product USING btree(price);
-
